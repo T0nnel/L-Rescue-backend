3a7b4f6098054fb7493df0509fe4ca96efd3cde9
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-12-10 05:50:56 +0300

    changing the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.json());
-  
-  // Enable CORS for specific origins
+
   app.enableCors({
-    origin: 'https://nextjs-boilerplate-five-opal-54.vercel.app', // Your frontend's origin
+    origin: 'https://nextjs-boilerplate-five-opal-54.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // If you need to send cookies or HTTP auth
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
-  
-  
+
   await app.listen(3001);
 }
-
 bootstrap();
